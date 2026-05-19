7a2d6b12c8966e783585c9f0de78b2f2e67dd4d3
asherKaltura <asher.desta@kaltura.com>
2026-05-19 13:42:16 +0300

    new project

--- src/main/java/flows/ShoppingFlow.java
+++ src/main/java/flows/ShoppingFlow.java
@@ -22,6 +22,7 @@ public class ShoppingFlow {
         this.cartPage = new CartPage(page);
     }
 
+
     @Step("Run full shopping flow: query={query}, maxPrice={maxPrice}, limit={limit}")
     public double run(String query, double maxPrice, int limit) {
 
